fa1ead67dcaa990f0d00b12390a854d9694baee8
emir <emir.jumaliev@gmail.com>
2019-10-19 01:55:40 -0400

    was added models Poll and Choice and was made migration

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Poll(models.Model):
+
+    question = models.TextField(max_length = 200, null = False, blank = False, verbose_name = 'Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+class Choice(models.Model):
+
+    text = models.TextField(max_length=200,null=False,blank=False,verbose_name='Ответ')
+    poll = models.ForeignKey('Poll',on_delete=models.CASCADE, null=True, blank=True, verbose_name='Вопрос',
+                                 related_name='choice_poll')
+
+
 # Create your models here.
